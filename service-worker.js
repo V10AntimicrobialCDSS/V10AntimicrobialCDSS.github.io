2c20057acb54d88ed5e1c9992d1a0ce8b426ba47
Stollings, Charles <Charles.Stollings@va.gov>
2026-03-26 11:24:55 -0400

    test

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const APP_PREFIX = 'CDSS_';
-const VERSION = '1.241'; // Update the version when you make changes
+const VERSION = '1.242'; // Update the version when you make changes
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const URLS = [
